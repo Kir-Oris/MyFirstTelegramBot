9c18137756fe257104f30e9db85305c0c28b9023
Kir Oris <slayerofshadow1.0@gmail.com>
2025-06-02 15:25:49 +0300

    Эхо-бот

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print('This is a file from GitHub repository')
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message, ContentType
+
+
+BOT_TOKEN = '7730391306:AAFaHDUhTiR4oGAVvgJ991Bvp0LDYj9RXPI'
+
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command(commands='start'))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+@dp.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение'
+    )
+
+
+@dp.message()
+async def send_echo(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+            'методом send_copy'
+        )
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
